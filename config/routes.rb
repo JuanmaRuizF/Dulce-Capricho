30be1215799a2156842b5179394c58716f111a09
mariarf <mariarf1478@gmail.com>
2021-04-15 12:25:46 +0100

    webpacker listo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'vistas_servidor/login'
-  get 'vistas_servidor/menu'
+  get 'vistas_servidor/menu', to: 'vistas_servidor#menu'
   root "vistas_clientes#index"
 
   get "/vistas_clientes", to: "vistas_clientes#index"
